a91dcf9eac8905f9406c9b8dea5f32215f4f6d2b
root <root@snf-51464.vm.okeanos-global.grnet.gr>
2020-10-07 01:43:21 -0400

    reminder front works, write logic

--- app/src/app/components/reminder/reminder.component.ts
+++ app/src/app/components/reminder/reminder.component.ts
@@ -5,7 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './reminder.component.html',
   styleUrls: ['./reminder.component.scss'],
 })
+
 export class ReminderComponent implements OnInit {
+
+  text:string = "";
+  todaysDate: String = new Date().toISOString();
+
+
+  //set default timer, probably same as before
+  time:string = "";
 
   constructor() { }
 
